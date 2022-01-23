5520dd15f41ca52a1c1368e71628bcfa15652cef
kristiinasaari <74016307+kristiinasaari@users.noreply.github.com>
2022-01-23 21:19:20 +0200

    lisäyksiä

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Button, Alert, TextInput, Text } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('');
+
+  const buttonPressed = () => { 
+    Alert.alert('You typed: ' + text); 
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Guess a number between 1-100</Text>
+      <TextInput style={styles.input} onChangeText={text => setText(text)} value={text}/>
+      <Button onPress={buttonPressed} title="MAKE GUESS" />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +24,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
   },
+  image : {
+    width: 250,
+    height: 100
+  },
+  input : {
+    width:200  , 
+    borderColor: 'gray', 
+    borderWidth: 1
+  }
 });
