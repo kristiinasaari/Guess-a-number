84b70a11575a1b4ffeec6665b9768d22e0da2c8e
kristiinasaari <74016307+kristiinasaari@users.noreply.github.com>
2022-01-28 19:54:30 +0200

    korjattu kirjoitusvirhe

--- App.js
+++ App.js
@@ -1,19 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, Button, Alert, TextInput, Text } from 'react-native';
 
-export default function App() {
-  const [text, setText] = useState('');
 
-  const buttonPressed = () => { 
-    Alert.alert('You typed: ' + text); 
-  };
+
+export default function App() {
+
+  const [input, setInput] = useState('');
+  const [guidance, setGuidance] = useState('');
+
+  const init = () => {
+    setGuidance('Guess a number between 1-100');
+    guesses = 0;
+    secret = Math.floor(Math.random() * 100) + 1;
+    console.log('Secret:', secret);
+  }
+
+  useEffect(() => {
+    init();
+  }, [])
+
+  const makeGuess = () =>  {
+    const guess = Number(input);
+    console.log('Guess:', guess);
+    guesses++;
+    if (guess < secret) {
+      setGuidance(`Your guess ${guess} is too low`);
+    } else if (guess > secret) {
+      setGuidance(`Your guess ${guess} is too high`)
+    } else {
+      Alert.alert(`You guessed the number in ${guesses} guesses`);
+      init();
+    }
+    setInput('');
+  }
 
   return (
     <View style={styles.container}>
-      <Text>Guess a number between 1-100</Text>
-      <TextInput style={styles.input} onChangeText={text => setText(text)} value={text}/>
-      <Button onPress={buttonPressed} title="MAKE GUESS" />
+      <Text style={styles.title}>{guidance}</Text>
+      <TextInput style={styles.input} value={input} keyboardType='number-pad' onChangeText={text => setInput(text)} />
+      <Button onPress={makeGuess} title="MAKE GUESS" />
       <StatusBar style="auto" />
     </View>
   );
